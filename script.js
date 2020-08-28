7ae652b600f7bfd66e0f1d5474be30cb5e58546f
Brady Trudeau <bradytrudeau@gmail.com>
2020-08-28 16:38:12 -0500

    Added addEmployee function with placeholders

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 $(document).ready(onReady);
 
+function addEmployee() {
+  console.log("in addEmployee");
+  const newEmployee = {
+    firstName: "First Name",
+    lastName: "Last Name",
+    id: "ID",
+    title: "Title",
+    annualSalary: "Annual Salary",
+  };
+}
+
 function onReady() {
   console.log("Ready To Go!");
+  // capture click event with addEmployeeButton
+  $("#addEmployeeButton").on("click", addEmployee);
 }
