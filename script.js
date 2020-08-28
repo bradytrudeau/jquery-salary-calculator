dcfb6d1d8c4b7864bd0cb69ca6d1bd6bdee878fa
Brady Trudeau <bradytrudeau@gmail.com>
2020-08-28 16:51:53 -0500

    Pushed newEmployee into a new array

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 $(document).ready(onReady);
 
+let employeeArray = [];
+
 function addEmployee() {
   console.log("in addEmployee");
+  //Create newEmployee based on values from inputs
   const newEmployee = {
-    firstName: "First Name",
-    lastName: "Last Name",
-    id: "ID",
-    title: "Title",
-    annualSalary: "Annual Salary",
+    firstName: $("#firstNameIn").val(),
+    lastName: $("#lastNameIn").val(),
+    id: $("#idIn").val(),
+    title: $("#titleIn").val(),
+    annualSalary: $("#annualSalaryIn").val(),
+    delete: null,
   };
+  //Push newEmployee parameters into table
+  console.log("New Employee:", newEmployee);
+  employeeArray.push(newEmployee);
+  console.log("Employee List:", employeeArray);
 }
 
 function onReady() {
