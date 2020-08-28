7626214e6622ea3ed1eb25cc6419c6bb9d0bea7c
Brady Trudeau <bradytrudeau@gmail.com>
2020-08-28 17:21:20 -0500

    Added style to table and connected it to JS

--- script.js
+++ script.js
@@ -11,12 +11,27 @@ function addEmployee() {
     id: $("#idIn").val(),
     title: $("#titleIn").val(),
     annualSalary: $("#annualSalaryIn").val(),
-    delete: null,
+    delete: null, //need to add delete button here
   };
   //Push newEmployee parameters into table
   console.log("New Employee:", newEmployee);
   employeeArray.push(newEmployee);
   console.log("Employee List:", employeeArray);
+  //Display employees in table
+  displayEmployees();
+}
+
+function displayEmployees() {
+  console.log("In displayEmployees");
+  let el = $("#employeeTable");
+  el.empty();
+  //Loop through employeeArray
+  for (let i = 0; i < employeeArray.length; i++) {
+    el.append(
+      `<td>${employeeArray[i].firstName}</td>`,
+      `<td>${employeeArray[i].lastName}</td>`
+    );
+  }
 }
 
 function onReady() {
